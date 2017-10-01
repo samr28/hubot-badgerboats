0a081468a422fbe051c49fc642c3e4a1abaa0951
samr28 <samroq@yahoo.com>
2017-10-01 00:40:33 -0500

    Add basic support for multiple admins

--- scripts/hubot-badgerboats.js
+++ scripts/hubot-badgerboats.js
@@ -12,6 +12,7 @@ const utils = require('./utils');
 const moment   = require('moment');
 
 const REDIS_BRAIN_KEY = "badgerboats";
+const REDIS_BRAIN_ADMINS_KEY = "badgerboatsAdmins";
 // Notified on bot start. Can be users or channels (make sure to use @|#)
 const NOTIFY_GROUPS = ['@sam'];
 const ADMIN_USER_NAME = process.env.HUBOT_ADMIN_USER_NAME || 'sam';
@@ -43,10 +44,17 @@ module.exports = function (robot) {
    */
   function initBrain() {
     console.log(`LOADED DATA: ${robot.brain.get(REDIS_BRAIN_KEY)}`);
+    console.log(`ADMINS DATA: ${robot.brain.get(REDIS_BRAIN_ADMINS_KEY)}`);
+    // TODO use a for loop here
     let brainData = robot.brain.get(REDIS_BRAIN_KEY);
     if (!brainData || !Array.isArray(brainData)) {
       console.log('NO PREV DATA');
       robot.brain.set(REDIS_BRAIN_KEY, [ADMIN_USER_NAME]);
+    }
+    let brainDataAdmins = robot.brain.get(REDIS_BRAIN_ADMINS_KEY);
+    if (!brainDataAdmins || !Array.isArray(brainDataAdmins)) {
+      console.log('NO PREV ADMIN DATA');
+      robot.brain.set(REDIS_BRAIN_ADMINS_KEY, [ADMIN_USER_NAME]);
     }
   }
 }
@@ -61,15 +69,16 @@ function handleMsg(robot, msg) {
   let message = msg.match[1];
   utils.logMsgData(msg, `HANDLE MESSAGE: ${message}`);
   let user = msg.message.user;
-  let isCommand = false;
-  if (user.id == ADMIN_USER_ID) {
+  let isCommand = true;
+  let admins = getAdmins(robot);
+  if (admins.indexOf(user.name) !== -1) {
     // Add is the first word (command)
     if (message.indexOf('!add') === 0) {
       isCommand = true;
       let addUser = message.slice(5);
       // Slice off "add" to just leave the user name
       msg.send(`Adding user: "${addUser}"`);
-      addMsgUser(robot, addUser);
+      addMsgUser(robot, msg, addUser);
     } else if (message.indexOf('!remove') === 0) {
       let brainData = robot.brain.get(REDIS_BRAIN_KEY);
       isCommand = true;
@@ -79,9 +88,22 @@ function handleMsg(robot, msg) {
     } else if (message.indexOf('!list') === 0) {
       isCommand = true;
       msg.send(`*User list*: ${getMsgUsers(robot).join(', ')}`);
+      msg.send(`*Admin list*: ${getAdmins(robot).join(', ')}`);
     } else if (message.indexOf('!up') === 0) {
       isCommand = true;
       msg.send(`I was started ${moment(startTime).fromNow()}`);
+    } else if (message.indexOf('!admin') == 0) {
+      isCommand = true;
+      let adminUser = message.slice(7);
+      addAdmin(robot, adminUser);
+    }
+    else if (message.indexOf('!help') === 0) {
+      isComand = true;
+      msg.send(`!add <user> - add a user
+!remove <user> - remove a user
+!list - list all users
+!up - get bot uptime
+!help - display this help`);
     }
   }
   if (!isCommand) {
@@ -110,6 +132,7 @@ function sendMessage(robot, msg, message) {
 /**
  * Get the array of message users from Redis brain
  * @param  {Object} robot Hubot object
+ * @return {Array}        User array
  */
 function getMsgUsers(robot) {
   let brainData = robot.brain.get(REDIS_BRAIN_KEY);
@@ -124,13 +147,16 @@ function getMsgUsers(robot) {
 
 /**
  * Add a user to the message list in Redis brainData
- * @param {Object} robot Hubot Object
- * @param {String} user  Username
+ * @param {Object}  robot Hubot Object
+ * @param  {Object} msg   Incoming messag
+ * @param {String}  user  Username
  */
-function addMsgUser(robot, user) {
+function addMsgUser(robot, msg, user) {
   let brainData = getMsgUsers(robot);
   brainData.push(user);
   robot.brain.set(REDIS_BRAIN_KEY, brainData);
+  let message = `_${user} had joined the party :party-parrot:_`;
+  sendMessage(robot, msg, message);
   robot.messageRoom(`@${user}`, `You've been added to badgerboats, welcome here!`);
 }
 
@@ -142,12 +168,40 @@ function addMsgUser(robot, user) {
 function removeMsgUser(robot, msg, user) {
   let brainData = getMsgUsers(robot);
   let index = brainData.indexOf(user);
-  msg.send(`${user} - ${brainData}`)
   if (index === -1) {
     return msg.send(`"${user}" is not on the list!`);
   }
   brainData.splice(index, 1);
   robot.messageRoom(`@${user}`, `You've been removed from badgerboats, see ya l8tr m8!`);
   return msg.send(`Removed "${user}" from the list`);
+}
 
+/**
+ * Get the array of message admins from Redis brain
+ * @param  {Object} robot Hubot object
+ * @return {Array}        User array
+ */
+function getAdmins(robot) {
+  let brainData = robot.brain.get(REDIS_BRAIN_ADMINS_KEY);
+  if (!Array.isArray(brainData)) {
+    return new Error(`Invalid data recieved from redis brain! Expected array but recieved ${typeof(brainData)}`);
+  }
+  if (brainData.length === 0) {
+    return new Error('No users in brain!');
+  }
+  return brainData;
+}
+
+/**
+ * Add a user to the list of adminUser
+ * @param {Object} robot Hubot Object
+ * @param {String} user  Username
+ */
+function addAdmin(robot, user) {
+  console.log(user);
+  let brainData = getMsgUsers(robot);
+  brainData.push(user);
+  robot.brain.set(REDIS_BRAIN_ADMINS_KEY, brainData);
+  robot.messageRoom(`@${user}`, `You are now an admin on badgerboats!
+  Type !help in chat to get started`);
 }
